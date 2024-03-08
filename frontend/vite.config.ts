58e9d6dcbc5ca8e83d4bb65cee79a697fadc76ae
Roland <machodin@yandex.ru>
2024-03-09 00:45:52 +0300

    fix env var frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './'
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    base: './',
+    define: {
+      VITE_API_URL: process.env.VITE_API_URL
+    }
+  };
 });
