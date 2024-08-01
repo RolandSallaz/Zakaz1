5b533eeffb33f3287d31c84dc8c8febb13da0732
Roland <machodin@yandex.ru>
2024-08-01 17:12:39 +0300

    debug

--- frontend/app/agreement/page.tsx
+++ frontend/app/agreement/page.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { getChapterInfo } from "../lib/services/api";
 import { INFOCHAPTER } from "../lib/utils/config";
 import InfoChapter from "../components/InfoChapter/InfoChapter";
-async function fetchData() {
-  try {
-    return await getChapterInfo(INFOCHAPTER.agreement.link);
-  } catch (error) {
-    console.error("Error fetching item:", error);
-    return null;
-  }
-}
 
 export default async function Page() {
+  async function fetchData() {
+    try {
+      return await getChapterInfo(INFOCHAPTER.agreement.link);
+    } catch (error) {
+      console.error("Error fetching item:", error);
+      return null;
+    }
+  }
   const content = await fetchData();
   if (!content) {
     return <div className="error">Ошибка загрузки данных.</div>;
